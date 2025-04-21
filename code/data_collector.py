965c2ff60171c0c2200d8ed8c687369f2f426f44
sophiaawang <sophiawang1206@gmail.com>
2025-04-20 21:40:21 -0400

    Adding data set collection

---
+++ code/data_collector.py
@@ -0,0 +1,34 @@
+import learn2learn as l2l
+from torchvision import transforms
+import os
+
+# learn2learn is a library for few-shot learning tasks
+# it will pre-split the dataset to have 64 base (train) classes, 16 validation classes,
+# and 20 novel (test) classes.
+
+
+def get_datasets(root="../data"):
+    data_path = os.path.join(os.path.dirname(__file__), root)
+    data_path = os.path.abspath(data_path)
+
+    transform = transforms.Compose(
+        [transforms.Resize(84), transforms.CenterCrop(84), transforms.ToTensor()]
+    )
+
+    train = l2l.vision.datasets.MiniImagenet(
+        root=data_path, mode="train", transform=transform
+    )
+    val = l2l.vision.datasets.MiniImagenet(
+        root=data_path, mode="validation", transform=transform
+    )
+    test = l2l.vision.datasets.MiniImagenet(
+        root=data_path, mode="test", transform=transform
+    )
+
+    return train, val, test
+
+
+# NOTE: example on what the dataset looks like:
+# img, label = train_dataset[0]
+# print(img.shape)   # torch.Size([3, 84, 84])
+# print(label)       # int (e.g., 0 to 63 in train, 0 to 15 in val, etc.)
